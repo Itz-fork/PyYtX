5f67d214ba90cc435ffd87b31cc13e2abc72e7ab
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-08-04 16:53:57 +0530

    updated to v0.5
    - Added Method to download thumbnails
    - Added Thumbnail download options
    - Added download feature to 'getthumb' method

--- PyYtX/ythumb.py
+++ PyYtX/ythumb.py
@@ -1,25 +1,39 @@
 # Copyright (c) 2021 Itz-fork
 # Part of PyYt Project
 
+import os
 import logging
+import wget
+
 from urllib.parse import urlparse, parse_qs
 
-logger = logging.getLogger(__name__)
+# Logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 class PyYtX:
-    def get_thumb(url=None):
+    def get_thumb(url=None, download=False, path=None):
         url = url
+        download = download
         if url is None:
             logging.error("Url Isn't Provided")
         # Getting Video Id from given Url
         logging.info("Getting Video ID From Url")
         Yt_Vid_Id = PyYtX.extract_video_id(url)
         if Yt_Vid_Id is None:
-            logging.error("Given Url is not a Youtube Url")
-        else:
+            logging.error("Given Url is not a Youtube Video Url")
+            return
+        try:
+            # Video Urls
             max_thumb = f"https://img.youtube.com/vi/{Yt_Vid_Id}/maxresdefault.jpg"
             standered_thumb = f"https://img.youtube.com/vi/{Yt_Vid_Id}/sddefault.jpg"
-            return max_thumb, standered_thumb
+            # Now processing url
+            if download is False:
+                return max_thumb, standered_thumb
+            elif download is True:
+                urls = [max_thumb, standered_thumb]
+                PyYtX.download_thumbs(urls, path)
+        except Exception as e:
+            logging.error(e)
     
     def extract_video_id(url):
         query = urlparse(url)
@@ -32,3 +46,17 @@ class PyYtX:
                 if query.path[:3] == '/v/': return query.path.split('/')[2]
         except:
             return None
+    
+    def download_thumbs(urls, path=None):
+        pyytx_urls = urls
+        # File Path
+        if path is not None:
+            path = path
+        else:
+            path = "pyytx_img"
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        pyytx_path = path
+        for imgu in pyytx_urls:
+            wget.download(imgu, out=pyytx_path)
+        logging.info("Download Finished")
